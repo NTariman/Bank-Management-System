6d7ce9de82ba6b3aa920635eeaa629bd9c226a9a
nTarimanMCM <100335139+NTariman@users.noreply.github.com>
2025-12-25 21:46:22 +0800

    Add main class with LoginPage GUI launch

---
+++ main.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @ Nathaniel Tariman
+ * 
+ *
+ */
+
+
+
+public class main {
+    public static void main(String[] args) {
+        // Launch the LoginPage GUI
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new LoginPage().setVisible(true);
+        });
+    }
+}
